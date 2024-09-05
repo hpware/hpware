00507de298b3eb339f952fe017844754703835d0
吳元皓 <157942818+hpware@users.noreply.github.com>
2024-09-05 18:52:57 +0800

    Update backup.sh

--- install/backup.sh
+++ install/backup.sh
@@ -1,3 +1,3 @@
 read -p "External Drive : " dirdrive
 cd /media/$USER/$dirdrive || exit 1
-sudo tar -cvpzf backup.tar.gz / --exclude /media/
+sudo tar -cvpzf backup.tar.gz / --exclude /media/ --exclude /usr/share/ollama/
